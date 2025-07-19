860978af7c62454de6b08086b1f00d0601bded19
nguyenhuucan <110796346+nguyenhuucan@users.noreply.github.com>
2025-07-19 18:03:20 +0700

    Delete clear_format.py

--- clear_format.py
+++
@@ -1,4 +0,0 @@
-def clear_format(text):
-    # Xóa mọi ký tự không phải chữ cái và số, đưa về chữ thường, cắt bỏ các khoảng trống dư thừa
-    text = re.sub(r'\W+', ' ', text)
-    return ' '.join(text.lower().split())
