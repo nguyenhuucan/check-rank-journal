b8fd0400a00d7335c78baa43002c137e5e651d7a
nguyenhuucan <110796346+nguyenhuucan@users.noreply.github.com>
2025-07-19 18:43:44 +0700

    ok

--- streamlit_main.py
+++ streamlit_main.py
@@ -693,7 +693,17 @@ if st.session_state['authenticated']:
                 st.write("📌 Cách tính phần trăm xếp hạng theo quy định TDTU")
                 st.dataframe(df, use_container_width=True, hide_index=True)
 
-                st.write("📌 Quy định về tiêu chí tuyển NCV cộng tác: đang cập nhật")
+                st.write("📌 Quy định về tiêu chí tuyển NCV cộng tác: đang cập nhật
+                1️⃣ Có học vị tiến sĩ
+                2️⃣ Có kế hoạch nghiên cứu phù hợp với định hướng đào tạo
+                3️⃣ Là tác giả đứng đầu/gửi bài:
+                * 4 công bố WoS (KHTN-KT)
+                * Hoặc 2 công bố WoS hoặc 4 Scopus (KHXH)
+                4️⃣ Có kế hoạch hợp tác công bố khoa học với nhân sự cơ hữu
+                5️⃣ Đến Trường làm việc ít nhất 1 lần nếu chưa từng đến
+                6️⃣ Không có dấu hiệu vi phạm liêm chính học thuật
+                7️⃣ Có thể làm các hoạt động khác theo phê duyệt của Trường                           
+                ")
                 st.write("📌 Biểu mẫu: đang cập nhật")
             else:
                 st.warning("🚫 Bạn chưa được phân quyền để xem tài liệu nội bộ Khoa Toán - Thống kê")
